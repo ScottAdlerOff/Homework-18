ba21cd3e4605332dcd0b4dfa03d4548ec367e8d4
Scott Adler <enclave38@gmail.com>
2023-05-16 18:04:50 +0300

    Домашняя работа №18

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String login = "user_name";
+        String password = "password123";
+        String confirmPassword = "password123";
+
+        try {
+            UserValidator.validate(login, password, confirmPassword);
+            System.out.println("Вход выполнен успешно.");
+        } catch (WrongLoginException | WrongPasswordException e) {
+            System.err.println("Вход не выполнен: " + e.getMessage());
+        }
     }
 }
